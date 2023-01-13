2aa8186462545615459ffc7644611670d039f159
BobHathaway2 <rlhathaway56@yahoo.com>
2023-01-12 20:39:19 -0700

    forgot the x

--- scripts/responsiveNav.js
+++ scripts/responsiveNav.js
@@ -1,12 +1,17 @@
-// const menuItems = document.querySelectorAll('.menu-item');
 const hamburgerMenu = document.querySelector('span');
 const section = document.querySelector('section');
 
 hamburgerMenu.addEventListener('click', () => {
     if (section.style.display === 'none') {
         section.style.display = 'inline';
+        hamburgerMenu.innerText = 'X';
     } else {
         section.style.display = 'none';
+        hamburgerMenu.class='close-menu';
+        hamburgerMenu.class='material-icons md-light';
+        hamburgerMenu.innerText = 'menu'
     }
 })
+hamburgerMenu.class='material-icons md-light';
+hamburgerMenu.innerText = 'menu'
 section.style.display = 'none';
