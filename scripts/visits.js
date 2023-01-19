8f1e633075db779437ed5e14841a2f50d1e9adac
BobHathaway2 <rlhathaway56@yahoo.com>
2023-01-19 11:45:32 -0700

    spice up first visit message

--- scripts/visits.js
+++ scripts/visits.js
@@ -1,5 +1,9 @@
 const visitsElement = document.querySelector(".visits");
 let visitCount = Number(window.localStorage.getItem("visit-count"));
 visitCount++;
-visitsElement.textContent = `Visit Count: ${visitCount}`;
 localStorage.setItem("visit-count", visitCount);
+if (visitCount == 1) {
+    visitsElement.textContent = 'Visit Count: Your FIRST visit!';
+} else {
+    visitsElement.textContent = `Visit Count: ${visitCount}`;
+}
