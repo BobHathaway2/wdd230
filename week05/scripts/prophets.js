a542b3c60523718300f30c92e0d0db120a8e0f3a
Bob Hathaway <113385106+BobHathaway2@users.noreply.github.com>
2023-01-30 13:36:38 -0700

    Initial Prophets checkin

---
+++ week05/scripts/prophets.js
@@ -0,0 +1,56 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+let results = null;
+
+async function getProphetData(url) {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayProphets(data.prophets);   
+    }
+}
+
+function suffix(order) {
+    let orderSuffix;
+    switch (order) {
+        case 1:
+            orderSuffix = 'st';
+            break;
+        case 2:
+            orderSuffix = 'nd';
+            break;
+        case 3:
+            orderSuffix = 'rd';
+            break;
+        default :
+            orderSuffix = 'th';
+            break;
+    }
+    return orderSuffix;
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement("section");
+        const fullName = document.createElement("h2");
+        const dob = document.createElement("p");
+        const pob = document.createElement("p");
+        const portrait = document.createElement("img");
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+        pob.textContent =  `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `The Prophet ${prophet.name} ${prophet.lastname}. ${prophet.order}${suffix(prophet.order)} Latter-day President`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");    
+        card.appendChild(fullName);
+        card.appendChild(dob);
+        card.appendChild(pob);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
+
+getProphetData(url);
